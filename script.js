7eed20766378410e6eef071b998891c2dd61fb93
tayyab203 <tayyabalibajwa123@gmail.com>
2020-04-24 15:02:41 +0500

    commit 11

--- script.js
+++ script.js
@@ -53,4 +53,25 @@ function checkLength(input, min, max) {
       showSuccess(input);
     }
   }
+  // Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+      showError(input2, 'Passwords do not match');
+    }
+  }
   
+  // Get fieldname
+  function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+  }
+  
+  // Event listeners
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+  });
