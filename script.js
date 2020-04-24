ca201f0062ba0d54e943a0656e586273297f27dd
tayyab203 <tayyabalibajwa123@gmail.com>
2020-04-24 15:01:16 +0500

    commit 8

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function showError(input, message) {
   const small = formControl.querySelector('small');
   small.innerText = message;
 }
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+  }
+  
+  // Check email is valid
+  function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+      showSuccess(input);
+    } else {
+      showError(input, 'Email is not valid');
+    }
+  }
+  
